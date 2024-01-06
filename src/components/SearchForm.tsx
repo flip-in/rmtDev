85c8982a9bc5cca1922d4079db420d2de802f0b8
Will Price <pricew86@gmail.com>
2024-01-06 10:39:59 +0700

    fix form submit bug

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -4,8 +4,8 @@ export default function SearchForm() {
   const { searchText, handleChangeSearchText } = useSearchTextContext();
 
   return (
-    <form action='#' className='search'>
-      <button type='submit' onSubmit={(e) => e.preventDefault()}>
+    <form action='#' className='search' onSubmit={(e) => e.preventDefault()}>
+      <button type='submit'>
         <i className='fa-solid fa-magnifying-glass'></i>
       </button>
 
